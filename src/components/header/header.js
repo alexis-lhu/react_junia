0612ed989c8d38d2aa58eaa3a93cc74378eb6a17
yandai <yan.dai@outlook.fr>
2024-11-25 16:33:15 +0100

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="Header-logo" alt="logo" />
+        <p>
+            Header
+        </p>
+    </header>
+  );
+}
+
+export default Header;
